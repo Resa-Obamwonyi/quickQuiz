b491cec2a7508cd2ec29f792a48ec6625312712c
Theresa <theresaobamwonyi@gmail.com>
2021-08-03 21:29:59 +0100

    Added high scores and json questions

--- end.js
+++ end.js
@@ -3,9 +3,9 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-// const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-// const MAX_HIGH_SCORES = 5;
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
@@ -16,14 +16,14 @@ username.addEventListener('keyup', () => {
 saveHighScore = (e) => {
     e.preventDefault();
 
-    // const score = {
-    //     score: mostRecentScore,
-    //     name: username.value,
-    // };
-    // highScores.push(score);
-    // highScores.sort((a, b) => b.score - a.score);
-    // highScores.splice(5);
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
 
-    // localStorage.setItem('highScores', JSON.stringify(highScores));
-    // window.location.assign('/');
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
 };
